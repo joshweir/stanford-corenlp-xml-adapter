e565a5344a5c75fdd1dbe70ff7511605a1738021
joshweir <joshua.weir@gmail.com>
2019-10-17 08:32:13 +1100

    Revert "add additional property to dep parse"
    This reverts commit 52cc9605990b49d5a75768a7d7f105434975ec58.

--- lib/stanford_corenlp_xml_adapter/coreference.rb
+++ lib/stanford_corenlp_xml_adapter/coreference.rb
@@ -1,92 +1,77 @@
 module Coreference
-  def coreferences(options = {})
-    self.xpath("//coreference//coreference").map { |c| coref_nok_to_blob(c, options) }
+  def coreferences
+    self.xpath("//coreference//coreference").map{|c| coref_nok_to_blob(c)}
   end
 
-  #def coreferences_with_pos
-  #  self.xpath("//coreference//coreference").map{|c| coref_nok_to_blob(c, true)}
-  #end
+  def coreferences_with_pos
+    self.xpath("//coreference//coreference").map{|c| coref_nok_to_blob(c, true)}
+  end
 
-  def coref_nok_to_blob(input, options = {})
+  def coref_nok_to_blob input, with_pos_tags=nil
     {
-      representative: coref_representative_nok_to_blob(input, options),
-      mentions: coref_mentions_nok_to_blob(input, options),
+      representative: coref_representative_nok_to_blob(input, with_pos_tags),
+      mentions: coref_mentions_nok_to_blob(input, with_pos_tags)
     }
   end
 
-  def coref_representative_nok_to_blob(input, options = {})
+  def coref_representative_nok_to_blob input, with_pos_tags=nil   
     coref_mention_nok_to_blob(
       input
-        .children
-        .select { |m|
-        m.name == "mention" && m.attributes["representative"]
+      .children
+      .select{|m| 
+        m.name == 'mention' && m.attributes['representative'] 
       }
-        .first,
+      .first,
       false,
-      options
+      with_pos_tags
     )
   end
 
-  def coref_mentions_nok_to_blob(input, options = {})
+  def coref_mentions_nok_to_blob input, with_pos_tags=nil
     input
-      .children
-      .select { |m|
-      m.name == "mention"
+    .children
+    .select{|m|
+      m.name == 'mention'
     }
-      .map { |m| coref_mention_nok_to_blob(m, true, options) }
+    .map{|m| coref_mention_nok_to_blob(m, true, with_pos_tags)}
   end
 
-  def coref_mention_nok_to_blob(input, add_representative_flag = nil, options = {})
-    add_representative_flag ?
+  def coref_mention_nok_to_blob input, add_representative_flag=nil, with_pos_tags=nil
+    add_representative_flag ? 
       with_representative_flag(
-      coref_mention(input, options),
-      input
-    ) :
-      coref_mention(input, options)
+        coref_mention(input, with_pos_tags),
+        input
+      ) : 
+      coref_mention(input, with_pos_tags)
   end
 
-  def coref_mention(input, options = {})
+  def coref_mention input, with_pos_tags=nil
     coref_blob = Hash[
       input
-        .children
-        .select { |v| v.class == Nokogiri::XML::Element }
-        .map { |v| [v.name.to_sym, to_i_based_on_field_name(v.text, v.name)] }
+      .children
+      .select{|v| v.class == Nokogiri::XML::Element}
+      .map{|v| [v.name.to_sym, to_i_based_on_field_name(v.text, v.name)]}
     ]
-    coref_blob = add_pos_tags_to(coref_blob) if options[:with_pos]
-    coref_blob = add_dep_parse_to(coref_blob) if options[:with_dep_parse]
-    coref_blob
+    with_pos_tags ? add_pos_tags_to(coref_blob) : coref_blob 
   end
 
-  def with_representative_flag(input, parent)
-    parent.name == "mention" && parent.attributes["representative"] ?
-      input.merge({representative: true}) : input
+  def with_representative_flag input, parent
+    parent.name == 'mention' && parent.attributes['representative'] ? 
+      input.merge({ representative: true }) : input
   end
 
-  def to_i_based_on_field_name(input, field_name)
-    field_name == "text" ? input : input.to_i
+  def to_i_based_on_field_name input, field_name
+    field_name == 'text' ? input : input.to_i
   end
 
-  def add_pos_tags_to(coref_blob)
+  def add_pos_tags_to coref_blob
     coref_blob.merge({
       pos: self
         .sentences[coref_blob[:sentence] - 1]
         .tokens
         .each_with_index
-        .select { |s, i| i >= coref_blob[:start] - 1 && i < coref_blob[:end] - 1 }
-        .map { |t, i| t.pos.text },
-    })
-  end
-
-  def add_dep_parse_to(coref_blob)
-    coref_blob.merge({
-      dep_parse: enhanced_plus_plus_dependencies(self, coref_blob[:sentence])
-      
-      self
-        .sentences[coref_blob[:sentence] - 1]
-        .tokens
-        .each_with_index
-        .select { |s, i| i >= coref_blob[:start] - 1 && i < coref_blob[:end] - 1 }
-        .map { |t, i| t.pos.text },
+        .select{|s, i| i >= coref_blob[:start] - 1 && i < coref_blob[:end] - 1 }
+        .map{|t, i| t.pos.text }
     })
   end
 end
--- lib/stanford_corenlp_xml_adapter/dependency_parse.rb
+++ lib/stanford_corenlp_xml_adapter/dependency_parse.rb
@@ -1,67 +1,66 @@
 module DependencyParse
-  def basic_dependencies(doc, sentence_index)
-    dependencies_for_type doc, sentence_index, "basic-dependencies"
+  def basic_dependencies doc, sentence_index
+    dependencies_for_type doc, sentence_index, 'basic-dependencies'
   end
 
-  def collapsed_dependencies(doc, sentence_index)
-    dependencies_for_type doc, sentence_index, "collapsed-dependencies"
+  def collapsed_dependencies doc, sentence_index
+    dependencies_for_type doc, sentence_index, 'collapsed-dependencies'
   end
 
-  def collapsed_ccprocessed_dependencies(doc, sentence_index)
-    dependencies_for_type doc, sentence_index, "collapsed-ccprocessed-dependencies"
+  def collapsed_ccprocessed_dependencies doc, sentence_index
+    dependencies_for_type doc, sentence_index, 'collapsed-ccprocessed-dependencies'
   end
 
-  def enhanced_dependencies(doc, sentence_index)
-    dependencies_for_type doc, sentence_index, "enhanced-dependencies"
+  def enhanced_dependencies doc, sentence_index
+    dependencies_for_type doc, sentence_index, 'enhanced-dependencies'
   end
 
-  def enhanced_plus_plus_dependencies(doc, sentence_index)
-    dependencies_for_type doc, sentence_index, "enhanced-plus-plus-dependencies"
+  def enhanced_plus_plus_dependencies doc, sentence_index
+    dependencies_for_type doc, sentence_index, 'enhanced-plus-plus-dependencies'
   end
 
-  def dependencies_for_type(doc, sentence_index, type)
+  def dependencies_for_type doc, sentence_index, type
     sentence = doc.sentences[sentence_index]
     sentence_tokens = sentence.tokens
     dependency_parse_nok_to_blob(
       sentence.xpath(".//dependencies[@type=\"#{type}\"]"),
-      sentence_tokens.map { |t| t.pos.text },
-      sentence_tokens.map { |t| t.ner.text },
+      sentence_tokens.map{|t| t.pos.text},
+      sentence_tokens.map{|t| t.ner.text},
     )
   end
 
-  def dependency_parse_nok_to_blob(dep_parse_input, pos_tags_input, ner_tags_input)
+  def dependency_parse_nok_to_blob dep_parse_input, pos_tags_input, ner_tags_input
     dep_parse_input
-      .children
-      .select { |dep| dep.name == "dep" }
-      .each_with_index
-      .map { |dep| dependency_nok_to_blob(dep, pos_tags_input, ner_tags_input) }
+    .children
+    .select{|dep| dep.name == 'dep'}
+    .each_with_index
+    .map{|dep| dependency_nok_to_blob(dep, pos_tags_input, ner_tags_input)}
   end
 
-  def dependency_nok_to_blob(dep_parse_input, pos_tags_input, ner_tags_input)
+  def dependency_nok_to_blob dep_parse_input, pos_tags_input, ner_tags_input
     {
-      type: dep_parse_input.attributes["type"].value,
-      extra: dep_parse_input.attributes["extra"].value,
+      type: dep_parse_input.attributes['type'].value,
       governor: dependency_slice_nok_to_blob(
-        "governor", dep_parse_input, pos_tags_input, ner_tags_input
+        'governor', dep_parse_input, pos_tags_input, ner_tags_input
       ),
       dependent: dependency_slice_nok_to_blob(
-        "dependent", dep_parse_input, pos_tags_input, ner_tags_input
-      ),
+        'dependent', dep_parse_input, pos_tags_input, ner_tags_input
+      )
     }
   end
 
-  def dependency_slice_nok_to_blob(name, dep_parse_input, pos_tags_input, ner_tags_input)
+  def dependency_slice_nok_to_blob name, dep_parse_input, pos_tags_input, ner_tags_input
     dep = dep_parse_input.children
-      .select { |dep| dep.name == name }
+      .select{|dep| dep.name == name}
       .first
-    idx = dep.attributes["idx"].value.to_i
+    idx = dep.attributes['idx'].value.to_i
     pos = idx > 0 ? pos_tags_input[idx - 1] : nil
-    ner = idx > 0 && ner_tags_input[idx - 1] != "O" ? ner_tags_input[idx - 1] : nil
+    ner = idx > 0 && ner_tags_input[idx - 1] != 'O' ? ner_tags_input[idx - 1] : nil
     {
       idx: idx,
       value: dep.text,
       pos: pos,
-      ner: ner,
+      ner: ner
     }
   end
 end
